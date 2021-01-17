8a843e4d587c9701cd12e6bbd95801f27873ed0c
OleksiiMaistriuk <65893898+OleksiiMaistriuk@users.noreply.github.com>
2021-01-17 17:26:35 +0100

    complite

--- src/index.js
+++ src/index.js
@@ -1,1 +1,90 @@
-import './styles.css';
+import './style.css';
+// import './classic';
+class Tabs {
+  constructor({
+    rootSelector,
+    activeControlClass = 'active',
+    activePaneClass = 'active',
+    activeTab = 1,
+  }) {
+    this._refs = this._getRefs(rootSelector);
+    this._activeControlClass = activeControlClass;
+    this._activePaneClass = activePaneClass;
+    this._activeTabIdx = activeTab - 1;
+    this._bindEvents();
+    this._setActiveTab();
+  }
+
+  _getRefs(root) {
+    const refs = {};
+
+    refs.controls = document.querySelector(`${root} [data-controls]`);
+    refs.panes = document.querySelector(`${root} [data-panes]`);
+    return refs;
+  }
+  _bindEvents() {
+    this._refs.controls.addEventListener(
+      'click',
+      this._onControlsClick.bind(this),
+    );
+  }
+  _onControlsClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'A') {
+      return;
+    }
+    this._removeActiveTab();
+    const controlItem = event.target;
+    controlItem.classList.add(this._activeControlClass);
+
+    const paneId = this._getPaneId(controlItem);
+
+    this._setActivePane(paneId);
+  }
+  _getPaneId(control) {
+    return control.getAttribute('href').slice(1);
+  }
+  _getPaneById(id) {
+    return this._refs.panes.querySelector(`#${id}`);
+  }
+  _setActiveTab() {
+    const controlItems = this._refs.controls.querySelectorAll('a');
+
+    const control = controlItems[this._activeTabIdx];
+
+    control.classList.add(this._activeControlClass);
+
+    const paneId = this._getPaneId(control);
+
+    this._setActivePane(paneId);
+  }
+  _removeActiveTab() {
+    const currentControlItem = this._refs.controls.querySelector(
+      `.${this._activeControlClass}`,
+    );
+
+    if (!currentControlItem) {
+      return;
+    }
+    currentControlItem.classList.remove(this._activeControlClass);
+
+    const paneId = this._getPaneId(currentControlItem);
+    this._removeActivePane(paneId);
+  }
+  _setActivePane(paneId) {
+    const pane = this._getPaneById(paneId);
+    pane.classList.add(this._activePaneClass);
+  }
+
+  _removeActivePane(paneId) {
+    const pane = this._getPaneById(paneId);
+    pane.classList.remove(this._activePaneClass);
+  }
+}
+
+const tabs1 = new Tabs({
+  rootSelector: '#tabs-1',
+  activeControlClass: 'controls__item--active',
+  activePaneClass: 'pane--active',
+  activeTab: 1,
+});
